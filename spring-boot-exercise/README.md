fdf80a60dbf4c3e62040cb55c90d9e2775a65ad8
Ashim Jung Khadka <ashim.jung.khadka@gmail.com>
2019-05-24 23:23:48 +0545

    H2 Database

--- spring-boot-exercise/README.md
+++ spring-boot-exercise/README.md
@@ -7,3 +7,6 @@
 - using http basic security
 - using form based security
 - using ApplicationListener - AbstractAuthenticationFailureEvent
+
+## Part 2
+- using H2 Database
---
+++ spring-boot-exercise/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table customer(
+    id int,
+    name varchar(50),
+    address varchar(50)
+);
+
+insert into customer (id, name, address) values(1, 'ashim', 'dhapasi');
+insert into customer (id, name, address) values(2, 'sanjeev', 'boudha');
