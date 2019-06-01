4b10ec5800e9aecd9badbe7d7ddadf53e5c29f6e
Ashim Jung Khadka <ashim.jung.khadka@gmail.com>
2019-06-02 00:13:04 +0545

    fix actuator issue

--- spring/actuator/src/main/java/io/ashimjk/actuator/ActuatorApplication.java
+++ spring/actuator/src/main/java/io/ashimjk/actuator/ActuatorApplication.java
@@ -1,10 +1,8 @@
 package io.ashimjk.actuator;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAdminServer
 @SpringBootApplication
 public class ActuatorApplication {
 
