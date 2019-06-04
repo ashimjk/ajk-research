84abbb6f0ce75a384e91559d88bb2f49c6eb66ce
Ashim Jung Khadka <ashim.jung.khadka@gmail.com>
2019-06-04 22:19:05 +0545

    config - order service

---
+++ spring/configuration/cloud-config/order-service/src/main/java/io/ashimjk/order/service/HelloController.java
@@ -0,0 +1,27 @@
+package io.ashimjk.order.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class HelloController {
+
+  @Value("${app.service.name}")
+  private String serviceName;
+
+  @Value("${app.shared.attribute}")
+  private String sharedAttribute;
+
+  @GetMapping("/service")
+  public String getServiceName() {
+    return "service name [" + this.serviceName + "]";
+  }
+
+  @GetMapping("/shared")
+  public String getSharedAttribute() {
+    return " application.properties [" + this.sharedAttribute + "]";
+  }
+}
---
+++ spring/configuration/cloud-config/order-service/src/test/java/io/ashimjk/order/service/OrderServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package io.ashimjk.order.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
