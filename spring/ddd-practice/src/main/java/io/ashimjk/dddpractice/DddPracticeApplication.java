1319d403bb62ce5d23070c901b0abfa69b41bfcc
Ashim Khadka <ashim.jung.khadka@gmail.com>
2019-09-01 20:01:25 +0545

    DDD Practice

---
+++ spring/ddd-practice/src/main/java/io/ashimjk/dddpractice/DddPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.ashimjk.dddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddPracticeApplication.class, args);
+    }
+
+}
