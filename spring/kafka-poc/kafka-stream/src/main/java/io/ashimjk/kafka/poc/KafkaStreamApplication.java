5522140e638adde3a15f10058e83200c4a87dc38
ashimjk <ashim.khadka@clusus.com>
2019-09-04 12:11:00 +0545

    Kafka POC

---
+++ spring/kafka-poc/kafka-stream/src/main/java/io/ashimjk/kafka/poc/KafkaStreamApplication.java
@@ -0,0 +1,13 @@
+package io.ashimjk.kafka.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamApplication.class, args);
+    }
+
+}
