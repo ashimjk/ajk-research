47d7ce1e0d4d51faf3fd80440a80968a51faa8d7
ashimjk <ashim.khadka@clusus.com>
2019-09-24 17:51:08 +0545

    Initial Websocket-Stomp

---
+++ spring/websocket-stomp/src/main/java/io/ashimjk/websocketstomp/WebsocketStompApplication.java
@@ -0,0 +1,13 @@
+package io.ashimjk.websocketstomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketStompApplication.class, args);
+    }
+
+}
