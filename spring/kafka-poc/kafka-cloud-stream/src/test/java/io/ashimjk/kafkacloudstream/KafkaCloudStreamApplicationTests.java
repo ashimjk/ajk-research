4c0c23ff0708ea7596276c27c0bb993f9d9f379d
ashimjk <ashim.khadka@clusus.com>
2019-10-02 13:25:46 +0545

    add spring cloud stream - kafka

---
+++ spring/kafka-poc/kafka-cloud-stream/src/test/java/io/ashimjk/kafkacloudstream/KafkaCloudStreamApplicationTests.java
@@ -0,0 +1,39 @@
+package io.ashimjk.kafkacloudstream;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class KafkaCloudStreamApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testKafkaMessage() {
+
+        Map<String, String> value = new HashMap<>();
+        value.put("value", "Hello!");
+
+        RestAssured
+                .given()
+                .baseUri("http://localhost:" + port)
+                .log()
+                .all()
+                .when()
+                .get("?value=Hello World")
+                .then()
+                .statusCode(200);
+    }
+
+}
