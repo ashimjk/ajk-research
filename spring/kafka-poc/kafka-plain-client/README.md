fce6957d646a12f8b601127de063b97d206426d8
Ashim Khadka <ashim.jung.khadka@gmail.com>
2019-10-05 12:44:53 +0545

    kafka plain client

---
+++ spring/kafka-poc/kafka-plain-client/README.md
@@ -0,0 +1,13 @@
+# Kafka Plain Client
+
+```shell script
+kafka-topics --zookeeper 192.168.0.100:2181 --list
+kafka-topics --zookeeper 192.168.0.100:2181 --create --topic first-topic --partitions 3 --replication-factor 1
+kafka-console-producer --broker-list 192.168.0.100:2181 --topic first-topic
+
+kafka-console-consumer --bootstrap-server localhost:9092 --topic first-topic
+kafka-console-consumer --bootstrap-server localhost:9092 --topic first-topic --group consumer-group
+
+docker run -it --network=host edenhill/kafkacat:1.5.0 -b kafka1:9092 -L
+docker run -it --network=host edenhill/kafkacat:1.5.0 -b 192.168.0.100:9092 -L
+```
---
+++ spring/kafka-poc/kafka-plain-client/src/main/java/io/ashimjk/kafka/plain/client/ProducerDemo.java
@@ -0,0 +1,35 @@
+package io.ashimjk.kafka.plain.client;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        String bootstrapServers = "localhost:9092";
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first-topic", "hello world");
+
+        // send data - asynchronous
+        producer.send(record);
+
+        // flush and close producer
+        producer.flush();
+        producer.close();
+    }
+
+}
