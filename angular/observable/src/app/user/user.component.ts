9291647c95134f56f3a0456394027f8d2b92d690
Ashim Khadka <ashim.jung.khadka@gmail.com>
2019-11-27 20:05:25 +0545

    feat: observable

---
+++ angular/observable/src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  userId: number;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => this.userId = +params.id
+    );
+  }
+
+}
