133026c31560186f130d00cd6a4e3b6e1847f1f0
Ashim Khadka <ashim.jung.khadka@gmail.com>
2019-11-27 21:24:07 +0545

    feat: observable

--- angular/observable/src/app/header/header.component.ts
+++ angular/observable/src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Observable, Subscriber} from 'rxjs';
+import {UserService} from '../shared/user.service';
+import {filter, map, scan} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-header',
@@ -7,10 +11,50 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private userService: UserService) {
   }
 
   ngOnInit() {
+    const myObservable = new Observable(
+      (subscriber: Subscriber<string>) => {
+        setTimeout(() => subscriber.next('first stage'), 2000);
+        setTimeout(() => subscriber.next('second stage'), 4000);
+        setTimeout(() => subscriber.complete(), 5000);
+        setTimeout(() => subscriber.next('this does not work'), 5000);
+      }
+    );
+    myObservable.subscribe(
+      (data: string) => console.log(data),
+      (msg: string) => console.log(msg),
+      () => console.log('completed')
+    );
+
+    this.userService.userActivated
+      .pipe(
+        filter(x => x % 2 === 0),
+        map(x => x + x),
+        scan((acc, x) => acc + x)
+      )
+      .subscribe(x => console.log('value: ' + x));
+
+    this.userService.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = !this.user1Activated;
+        } else if (id === 2) {
+          this.user2Activated = !this.user2Activated;
+        }
+      },
+      () => {
+      },
+      () => {
+        this.user1Activated = false;
+        this.user2Activated = false;
+      }
+    );
   }
 
 }
--- angular/observable/src/app/user/user.component.ts
+++ angular/observable/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {UserService} from '../shared/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,8 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class UserComponent implements OnInit {
   userId: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private userService: UserService) {
   }
 
   ngOnInit() {
@@ -18,4 +20,11 @@ export class UserComponent implements OnInit {
     );
   }
 
+  onActivate() {
+    this.userService.userActivated.next(this.userId);
+  }
+
+  onDeactivate() {
+    this.userService.userActivated.complete();
+  }
 }
