251ddae0bcb393ada79f0aa75eeaa15207dedeb9
ashimjk <ashim.khadka@clusus.com>
2019-12-22 19:48:00 +0545

    feat: api-gateway

---
+++ spring/api-gateway/src/main/java/io/ashimjk/gateway/GatewayApplication.java
@@ -0,0 +1,30 @@
+package io.ashimjk.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public static class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+
+    }
+
+}
