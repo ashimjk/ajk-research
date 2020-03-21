2caae3a0ff21633c2d255b6366f3284975b0121a
ashimjk <ashim.khadka@clusus.com>
2020-03-21 18:11:42 +0545

    feat: add spring cloud git configuration

---
+++ spring/spring-cloud/git-config/src/main/java/io/ashimjk/spring/cloud/git/config/GitConfigApplication.java
@@ -0,0 +1,13 @@
+package io.ashimjk.spring.cloud.git.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitConfigApplication.class, args);
+    }
+
+}
---
+++ spring/spring-cloud/git-config/src/test/java/io/ashimjk/spring/cloud/git/config/GitConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ashimjk.spring.cloud.git.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
