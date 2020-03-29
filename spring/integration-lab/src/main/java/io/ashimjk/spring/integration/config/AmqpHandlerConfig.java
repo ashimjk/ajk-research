17de42fe0fe7f933e309a5e208491891d65100ed
ashimjk <ashim.khadka@clusus.com>
2020-03-30 00:15:32 +0545

    fix: amqp data conversion

--- spring/integration-lab/src/main/java/io/ashimjk/spring/integration/config/AmqpHandlerConfig.java
+++ spring/integration-lab/src/main/java/io/ashimjk/spring/integration/config/AmqpHandlerConfig.java
@@ -1,8 +1,11 @@
 package io.ashimjk.spring.integration.config;
 
+import io.ashimjk.spring.integration.domain.Beneficiary;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.ClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,6 @@ import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.json.ObjectToJsonTransformer;
 
 @Configuration
 @EnableIntegration
@@ -21,7 +23,6 @@ public class AmqpHandlerConfig {
     @Bean
     public IntegrationFlow amqpOutputIntegrationFlow(AmqpTemplate amqpTemplate) {
         return IntegrationFlows.from("amqpOutputChannel")
-                .transform(new ObjectToJsonTransformer())
                 .handle(Amqp.outboundAdapter(amqpTemplate)
                         .exchangeName("fileExchange")
                         .routingKey("fileQueue")
@@ -41,15 +42,29 @@ public class AmqpHandlerConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
     public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate amqpTemplate = new RabbitTemplate(connectionFactory);
         amqpTemplate.setMessageConverter(jackson2JsonMessageConverter());
         return amqpTemplate;
+    }
+
+    private Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        Jackson2JsonMessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        jackson2JsonMessageConverter.setClassMapper(getClassMapper());
+        return jackson2JsonMessageConverter;
+    }
+
+    private ClassMapper getClassMapper() {
+        return new ClassMapper() {
+            @Override
+            public void fromClass(Class<?> clazz, MessageProperties properties) {
+            }
+
+            @Override
+            public Class<?> toClass(MessageProperties properties) {
+                return Beneficiary.class;
+            }
+        };
     }
 
 }
