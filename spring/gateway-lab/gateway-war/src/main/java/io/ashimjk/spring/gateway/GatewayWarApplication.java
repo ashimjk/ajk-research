94453fd36b0cea21208909ceb8cc72c112f0f63f
ashimjk <ashim.khadka@clusus.com>
2020-04-10 18:33:54 +0545

    feat: add gateway war

---
+++ spring/gateway-lab/gateway-war/src/main/java/io/ashimjk/spring/gateway/GatewayWarApplication.java
@@ -0,0 +1,54 @@
+package io.ashimjk.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class GatewayWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWarApplication.class, args);
+    }
+
+    //@Bean
+    //public ServerCodecConfigurer serverCodecConfigurer() {
+    //    return ServerCodecConfigurer.create();
+    //}
+
+    @Bean
+    public GlobalFilter customFilter() {
+        return new GatewayServiceFilter();
+    }
+
+    public static class GatewayServiceFilter implements GlobalFilter, Ordered {
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            HttpHeaders headers = exchange.getResponse().getHeaders();
+
+            headers.set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+            headers.set("Pragma", "no-cache");
+            headers.set("X-Content-Type-Options", "nosniff");
+            headers.set("Strict-Transport-Security", "max-age=31536000 ; includeSubDomains");
+            headers.set("X-Frame-Options", "DENY");
+            headers.set("X-XSS-Protection", "1; mode=block");
+
+            return chain.filter(exchange);
+        }
+
+        @Override
+        public int getOrder() {
+            return -1;
+        }
+
+    }
+
+}
---
+++ spring/gateway-lab/gateway-war/src/test/java/io/ashimjk/spring/gateway/GatewayWarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ashimjk.spring.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
