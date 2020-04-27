40ae6492383b688633ffc8e79eafb0b9a4db082b
ashimjk <ashim.khadka@clusus.com>
2020-04-27 23:06:44 +0545

    feat: add spring-rabbitmq

---
+++ spring/spring-rabbitmq/src/main/java/io/ashimjk/spring/rabbitmq/SpringRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package io.ashimjk.spring.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqApplication.class, args);
+    }
+
+}
---
+++ spring/spring-rabbitmq/src/test/java/io/ashimjk/spring/rabbitmq/SpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ashimjk.spring.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
